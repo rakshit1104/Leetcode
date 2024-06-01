8303ce47f59436d1903195c8287025567941579f
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-01 16:30:43 +0530

    Time: 17 ms (18.41%) | Memory: 50.5 MB (5.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("\\s", "");
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.replaceAll(" ", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", ""); //to remove all special characters
         s = s.toLowerCase();
         return check(s, 0);
     }
