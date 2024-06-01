23fb0ae830ea927d3a4fa4a69c4b4e7959d5c613
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-01 16:25:31 +0530

    Time: 19 ms (15.99%) | Memory: 50.2 MB (5.66%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("\\s", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        return check(s, 0);
+    }
+
+    boolean check(String s, int i){
+        if(i >= s.length()/2) return true;
+        if(s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+        return check(s, i+1);
+    }
+}
