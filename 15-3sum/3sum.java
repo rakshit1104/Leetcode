718217b208f17bbcb7fdd9997ed15a03a5a36c6d
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-09 23:41:55 +0530

    Time: 26 ms (92.99%) | Memory: 52.4 MB (26.49%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 0; i < n; i++){
-            if(i != 0 && nums[i] == nums[i-1]) continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
             int j = i+1;
             int k = n-1;
@@ -17,8 +17,14 @@ class Solution {
                 } else if(sum > 0){
                     k--;
                 } else{
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    // List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    // ans.add(temp);
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
                     ans.add(temp);
+                    
                     j++;
                     k--;
                     while(j < k && nums[j] == nums[j-1]) j++;
