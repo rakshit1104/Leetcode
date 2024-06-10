0d54720a15501e6f5fb51410d26f3cbcf7c1a9fc
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-10 18:42:12 +0530

    Time: 2 ms (88.93%) | Memory: 41.2 MB (65.80%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int arr[] = new int[n];
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            arr[i] = heights[i];
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] != heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
