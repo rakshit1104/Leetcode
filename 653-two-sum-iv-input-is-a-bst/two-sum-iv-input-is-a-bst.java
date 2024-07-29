046ade61484e5e6a804496419afda8469d0b0dda
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-07-29 23:34:58 +0530

    Time: 23 ms (6.43%) | Memory: 45.2 MB (27.92%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,86 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    List<Integer> l=new ArrayList<>();
     public boolean findTarget(TreeNode root, int k) {
-        BstIteratorInorder inorder = new BstIteratorInorder(root);
-        BstIteratorInorderReverse inorderReverse = new BstIteratorInorderReverse(root);
-
-        int l = inorder.next();
-        int r = inorderReverse.next();
-
-        while (l < r && inorder.hasNext() && inorderReverse.hasNext()) {
-            if (l + r == k) {
-                return true;
-            } else if (l + r < k) {
-                l = inorder.next();
-            } else {
-                r = inorderReverse.next();
-            }
-        }
-        return false;
+        return help(root,k);
     }
-}
-
-class BstIteratorInorder {
-    Stack<TreeNode> stack;
-    BstIteratorInorder(TreeNode root) {
-        stack = new Stack<>();
-        pushIntoStack(root);
-    }
-
-    public boolean hasNext() {
-        return !stack.isEmpty();
-    }
-
-    public int next() {
-        TreeNode node = stack.pop();
-        if (node != null)
-            pushIntoStack(node.right);
-        return stack.isEmpty() ? -1 : node.val;
-    }
-    private void pushIntoStack(TreeNode node) {
-        while (node != null) {
-            stack.push(node);
-            node = node.left;
-        }
-    }
-}
-
-class BstIteratorInorderReverse {
-
-    Stack<TreeNode> stack;
-    BstIteratorInorderReverse(TreeNode root) {
-        stack = new Stack<>();
-        pushIntoStack(root);
-    }
-    public boolean hasNext() {
-        return !stack.isEmpty();
-    }
-
-    public int next() {
-        TreeNode node = stack.pop();
-        if (node != null)
-            pushIntoStack(node.left);
-        return stack.isEmpty() ? -1 : node.val;
-    }
-
-    private void pushIntoStack(TreeNode node) {
-        while (node != null) {
-            stack.push(node);
-            node = node.right;
-        }
+    
+    public boolean help(TreeNode root,int k )
+    {
+        if(root==null)return false;
+        if(l.contains(k-root.val))return true;
+        l.add(root.val);
+        return help(root.left,k)||help(root.right,k);
     }
 }
