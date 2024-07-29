b457096db8e487e10df164ca924882bea57f4fe7
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-07-29 23:37:12 +0530

    Time: 2 ms (95.00%) | Memory: 45.5 MB (8.22%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,14 +1,30 @@
 class Solution {
-    List<Integer> l=new ArrayList<>();
-    public boolean findTarget(TreeNode root, int k) {
-        return help(root,k);
+    public void getInorder(TreeNode root,ArrayList<Integer>arr){
+        if(root==null){
+            return;
+        }
+        getInorder(root.left,arr);
+        arr.add(root.val);
+        getInorder(root.right,arr);
     }
-    
-    public boolean help(TreeNode root,int k )
-    {
-        if(root==null)return false;
-        if(l.contains(k-root.val))return true;
-        l.add(root.val);
-        return help(root.left,k)||help(root.right,k);
+
+    public boolean findTarget(TreeNode root, int k) {
+        ArrayList<Integer>arr=new ArrayList<>();
+        getInorder(root,arr);
+
+        int i = 0;
+        int j = arr.size()-1;
+        while(i < j){
+            if(arr.get(i)+arr.get(j) == k){
+                return true;
+            } else if(arr.get(i)+arr.get(j) > k){
+                j--;
+            } else{
+                i++;
+            }
+
+        }
+        
+    return false;  
     }
 }
