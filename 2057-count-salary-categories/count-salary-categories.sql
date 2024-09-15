ed971fdd714632615870a2a203532825fd0618ba
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-09-15 19:19:47 +0530

    Time: 1737 ms (43.90%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,11 +1,11 @@
 SELECT 'Low Salary' AS category, 
-       COUNT(if(income<20000,1,null)) AS accounts_count
+    SUM(if(income<20000,1,0)) AS accounts_count
 FROM Accounts
 UNION
 SELECT 'Average Salary', 
-       COUNT(if(income>=20000 and income<=50000,1,null))
+   SUM(if(income>=20000 and income<=50000,1,0))
 FROM Accounts
 UNION
 SELECT 'High Salary', 
-       COUNT(if(income>50000,1,null))
+   SUM(if(income>50000,1,0))
 FROM Accounts;
